f231324f46c4894263d86f0938f58af074b5f0aa
Raul Velazquez <raul.velazquez@infomedia.com.mx>
2026-02-14 20:06:58 -0600

    funciones sobre funciones 2

--- src/main/scala/FuncionesPrimeraClase.scala
+++ src/main/scala/FuncionesPrimeraClase.scala
@@ -12,5 +12,17 @@ object  FuncionesPrimeraClase extends App {
   //List(1,2,3).map(funcionCuadratica(_)).foreach(println)
 
   //Funciones que reciben otras funciones
-  List(1,2,3).filter((x: Int) => x>1).foreach(println)
+  //List(1,2,3).filter((x: Int) => x>1).foreach(println)
+
+  //Funciones que retornan otras funciones
+  def saludar(prefix: String) = {
+    (name: String) => println(s"$prefix $name")
+  }
+
+  println(saludar("Hola"))
+  saludar("Hola")("Silvia")
+
+  val Buenos_dias = saludar("Buenos_dias")
+  Buenos_dias("Gabriel")
+
 }
