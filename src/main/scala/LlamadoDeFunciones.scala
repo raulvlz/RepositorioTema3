382942d97e56db28ffa2a4bf6832a44d762b68a0
Raul Velazquez <raul.velazquez@infomedia.com.mx>
2026-02-15 15:56:15 -0600

    Overloading

--- src/main/scala/LlamadoDeFunciones.scala
+++ src/main/scala/LlamadoDeFunciones.scala
@@ -3,9 +3,27 @@ object LlamadoDeFunciones extends App {
   //Llamado directo de la funcón
   def greet(prefix: String, name: String) = println(s"$prefix $name")
 
-  greet("Hola","Raul")
+  greet("Hola", "Raul")
 
-  //Llamado
-  greet(name="Raul", prefix="Hola")
+  //Llamado por caracteristicas
+  greet(name = "Raul", prefix = "Hola")
+
+
+
+
+  //Overloading
+  def greet2(prefix: String, name: String): String = s"$prefix $name"
+  def greet2(name: String): String = {
+    greet2("Hola", name)
+  }
+
+  println(greet2("Hola","Kevin"))
+
+
+
+
+
+
+
 
 }
